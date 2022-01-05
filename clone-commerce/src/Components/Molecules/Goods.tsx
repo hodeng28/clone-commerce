76d87133631ca4e90e013caf7e56640b9a8e2803
hodeng28 <ghepd28@gmail.com>
2022-01-05 23:38:45 +0900

    feat: goods markup

---
+++ clone-commerce/src/Components/Molecules/Goods.tsx
@@ -0,0 +1,37 @@
+import './Goods.scss';
+
+const Goods = () => (
+  <>
+    <div className="goods-container">
+      <div className="goods-area">
+        <div className="goods-figure">
+            <img 
+                alt="손잡이"
+                src="//file.moongori.com/firstmall/data/goods/1/93703/2021/04/tmp_809ef1f29800983c526ed0548febc6da3015list1.jpg" 
+            />
+        </div>
+        <div className="goods-inform">
+          <div className="goods-title">
+            <span>국민손잡이］방문손잡이 ATL-711(4컬러)</span>
+          </div>
+          <div className="goods-price">
+            <span className="sale">
+              <span>30</span>
+              <span>%</span>
+            </span>
+            <span className="price">
+              <span>11,000</span>
+              <span>원</span>
+            </span>
+          </div>
+          <div className="goods-rating">
+              
+              {/* <span>(100)</span> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+)
+
+export default Goods;
--- clone-commerce/src/Components/Pages/MainPage.tsx
+++ clone-commerce/src/Components/Pages/MainPage.tsx
@@ -1,11 +1,11 @@
-// import Goods from '../Molecules/Goods';
-
 import Category from '../Molecules/Category';
+import Goods from '../Molecules/Goods';
 
 const MainPage = () => (
     <>
         <div>
             <h4>오늘의 인기상품</h4>
+            <Goods />
         </div>
         <div>
             <h4>즐겨찾는 카테고리</h4>
